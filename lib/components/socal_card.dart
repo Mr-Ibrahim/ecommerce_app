0a7ade623a9ffc3a68b68c42d34a212f3bde7b02
Mr-Ibrahim <ibrahimabdulkhaliq94@gmail.com>
2021-01-25 07:02:22 +0500

    Ecommerce app

---
+++ lib/components/socal_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app/constants.dart';
+
+import '../size_config.dart';
+
+class SocalCard extends StatelessWidget {
+  const SocalCard({
+    Key key,
+    this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String icon;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+        height: getProportionateScreenHeight(60),
+        width: getProportionateScreenWidth(60),
+        decoration: BoxDecoration(
+          color: kSecondaryColor,
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
